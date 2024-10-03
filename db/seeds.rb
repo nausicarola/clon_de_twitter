9888497c5ccab896b9ecd8da85d464748c5037b8
nausicarola <carola.pulgar.soto@gmail.com>
2024-10-03 14:41:00 -0300

    agregar faker y poblar base de datos

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+20.times do
+  Tweet.create(
+    description: Faker::Lorem.sentence(word_count: 10),
+    user_name: Faker::Internet.username(specifier: 5..8)
+  )
+end
