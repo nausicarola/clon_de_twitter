9468f453135402a7153caf3e36db1fc771140317
nausicarola <carola.pulgar.soto@gmail.com>
2024-10-04 20:57:16 -0300

    Validaciones y estilos

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,5 @@
 class Tweet < ApplicationRecord
+   # Validaciones
+   validates :user_name, presence: true, length: { maximum: 50 }
+   validates :description, presence: true, length: { maximum: 280 }
 end
